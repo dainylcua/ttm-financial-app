f00e973bfbd305f4cfc5779a885ae06e0a55d3fb
dainylcua <dainylcua@gmail.com>
2022-02-12 17:35:42 -0600

    Adds max amount warning to main screen

--- components/BigNumber.tsx
+++ components/BigNumber.tsx
@@ -5,27 +5,32 @@ const BigNumber: React.FC = () => {
   const { amount, setAmount, } = useAmountContext()
 
   return (
-    <div className="flex flex-row items-center justify-center py-2 my-16">
-      <button 
-        onClick={() => setAmount((prev:number) => prev -= 1)}
-        className="text-6xl cursor-pointer select-none"
-      >
-        -
-      </button>
-      <Link href="/amount" passHref>
-        <a
-          className="flex flex-row items-center justify-center w-40 text-7xl"
+    <div className="flex flex-col py-2 mt-16 mb-10">
+      <div className="flex flex-row items-center justify-center">
+        <button 
+          onClick={() => setAmount((prev:number) => prev -= 1)}
+          className="text-6xl cursor-pointer select-none"
         >
-          <span className="self-start text-2xl">$</span>
-          {amount}
-        </a>
-      </Link>
-      <button 
-        onClick={() => setAmount((prev:number) => prev += 1)}
-        className="text-6xl cursor-pointer select-none"
-      >
-      +
-      </button>
+          -
+        </button>
+        <Link href="/amount" passHref>
+          <a
+            className="flex flex-row items-center justify-center w-40 text-7xl"
+          >
+            <span className="self-start text-2xl">$</span>
+            {amount}
+          </a>
+        </Link>
+        <button 
+          onClick={() => setAmount((prev:number) => prev += 1)}
+          className="text-6xl cursor-pointer select-none"
+        >
+        +
+        </button>
+      </div>
+      <div className={`${amount == 999 ? 'visible' : 'invisible'} my-2 text-red-500 opacity-70 animate-pulse ease-in-out`}>
+        Maximum transferable amount is $999.
+      </div>
     </div>
   )
 }
