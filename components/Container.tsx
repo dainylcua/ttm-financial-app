085228a0cde102aae7726413d9e65799a5f91788
dainylcua <dainylcua@gmail.com>
2022-02-12 10:09:16 -0600

    Adds base styling

---
+++ components/Container.tsx
@@ -0,0 +1,16 @@
+
+interface Props {
+  children?: React.ReactNode
+}
+
+const Container: React.FC<Props> = ({children}) => {
+  return (
+    <div className="flex flex-col w-full h-full min-h-screen bg-gray-200">
+      <div className="flex-1 mx-auto w-[350px] my-auto max-h-[40rem] bg-neutral-50 border shadow-xl border-gray-100 shadow-gray-400 rounded-xl p-8">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Container
