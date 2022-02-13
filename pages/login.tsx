b92bf13acf238190dc8a41a9a870486557a96099
dainylcua <dainylcua@gmail.com>
2022-02-13 12:32:37 -0600

    fixes buttons again

--- pages/login.tsx
+++ pages/login.tsx
@@ -80,7 +80,7 @@ const Login: NextPage = () => {
             <input 
               className="flex flex-col items-center justify-center w-24 h-10 text-lg font-bold text-center transition-all ease-in-out cursor-pointer select-none hover:text-md drop-shadow-lg bg-sky-600 rounded-xl text-gray-50 active:scale-90 active:bg-sky-700" 
               type="submit" 
-              value="Sign Up" 
+              value="Log In" 
             />
           </form>
         </div>
